1771a597e424ad543f01cf6ec164b2c25f94d49e
Justin <justintenser@me.com>
2017-01-16 15:23:32 -0500

    finished 1

---
+++ exercise1.rb
@@ -0,0 +1,14 @@
+puts 2
+puts 3
+puts 2 + 3
+
+puts "Hello\t\tworld"
+puts "hello\b\b\b\b\bGoodbye world"
+puts "Hello\rStart over world"
+puts "1. Hello\n2. World"
+
+(1 < 3) && (3 < 5)
+# true
+
+(1 > 1) && (2 > 2)
+#false 
