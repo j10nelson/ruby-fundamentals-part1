319eeee48fec8c983d0fe7b95a4161ef781ecd15
Justin <justintenser@me.com>
2017-01-16 20:53:42 -0500

    exercise2

---
+++ exercise2.rb
@@ -0,0 +1,7 @@
+meal = 55
+tip = meal * 0.15
+puts "a tip for a meal that costs $#{meal} is $#{tip}"
+
+puts "the result of 45628 * 7839 is #{45628 * 7839}"
+
+puts (10 < 20 && 30 < 20) || !(10 == 11)
